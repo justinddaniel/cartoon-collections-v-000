4c7e57b1b4f98b1958c4a9918e96f6bb19205c17
Justin Daniel <justinddaniel@gmail.com>
2017-09-19 18:14:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(veggies)
-  veggies.map { |veggie| veggie.foo[0].upcase + "!" }
+  veggies.map { |veggie| veggie.chars.first.upcase + "!"
 end
 
 def long_planeteer_calls# code an argument here
